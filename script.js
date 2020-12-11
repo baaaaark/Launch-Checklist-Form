187fa322592280e79185de2bbbe710b833dbeee9
Jason Oliver <jason@pop-os.localdomain>
2020-12-11 09:59:07 -0600

    Initial Commit

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+document.addEventListener("load", function() {
+   let launchForm = document.querySelector("form");
+
+   launchForm.addEventListener("submit", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      console.log(pilotName);
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === 0 || cargoMass.value === 0){
+         alert("All fields are required!");
+            // stop the form submission
+         console.log(copilotName);
+      }
+   });
+});
 // Write your JavaScript code here!
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
